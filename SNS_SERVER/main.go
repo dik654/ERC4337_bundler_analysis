40860d21ebee109d1bfbb3eae8838e515d29fa97
daeikkim <12377dl@gmail.com>
2023-12-06 15:18:46 +0900

    feat: main함수 작성완료

--- SNS_SERVER/main.go
+++ SNS_SERVER/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
 
+	"github.com/dik654/Go_projects/SNS_SERVER/controllers"
+	"github.com/dik654/Go_projects/SNS_SERVER/services"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server         *gin.Engine
+	userservice    services.UserService
+	usercontroller controllers.UserController
+	ctx            context.Context
+	usercollection *mongo.Collection
+	mongoclient    *mongo.Client
+	err            error
+)
+
+func init() {
+	ctx = context.Background()
+
+	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoclient, err = mongo.Connect(ctx, mongoconn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = mongoclient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("mongo connection established")
+
+	usercollection = mongoclient.Database("userdb").Collection("users")
+	userservice = services.NewUserService(usercollection, ctx)
+	usercontroller = controllers.New(userservice)
+	server = gin.Default()
+}
+
+func main() {
+	defer mongoclient.Disconnect(ctx)
+
+	basepath := server.Group("/v1")
+	usercontroller.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
 }
