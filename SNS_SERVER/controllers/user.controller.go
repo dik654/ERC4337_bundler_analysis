c118c9f6af08df3b558cd5a0976c4d34895fff42
daeikkim <12377dl@gmail.com>
2023-12-06 14:51:50 +0900

    feat: 회원정보 CRUD 코드 작성완료

--- SNS_SERVER/controllers/user.controller.go
+++ SNS_SERVER/controllers/user.controller.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"context"
+	"net/http"
 
+	"github.com/dik654/Go_projects/SNS_SERVER/models"
 	"github.com/dik654/Go_projects/SNS_SERVER/services"
 	"github.com/gin-gonic/gin"
 )
@@ -19,23 +21,53 @@ func New(userservice services.UserService) UserController {
 }
 
 func (uc *UserController) CreateUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	if err := uc.UserService.CreateUser(&user); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) GetUser(ctx *gin.Context) {
+	username := ctx.Param("name")
+	uc.UserService.GetUser(&username)
 	ctx.JSON(200, "")
 }
 
 func (uc *UserController) GetAll(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	users, err := uc.UserService.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
 }
 
 func (uc *UserController) UpdateUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	if err := uc.UserService.UpdateUser(&user); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) DeleteUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	username := ctx.Param("name")
+	if err := uc.UserService.DeleteUser(&username); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
@@ -44,5 +76,5 @@ func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
 	userroute.GET("/get/:name", uc.GetUser)
 	userroute.GET("/getall", uc.GetAll)
 	userroute.PATCH("/update", uc.UpdateUser)
-	userroute.DELETE("/delete", uc.DeleteUser)
+	userroute.DELETE("/delete/:name", uc.DeleteUser)
 }
