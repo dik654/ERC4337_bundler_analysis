53ac25322b9732e97d4f49ca3880f623a31eb320
daeikkim <12377dl@gmail.com>
2023-12-19 13:30:26 +0900

    feat: nestjs RPC QR 로직 추가

--- gRPC/qrcode_microservice/go_server/go.mod
+++ gRPC/qrcode_microservice/go_server/go.mod
@@ -2,4 +2,9 @@ module github.com/dik654/Go_projects/gRPC/qrcode_microservice/go_server
 
 go 1.21.4
 
-require github.com/golang-jwt/jwt/v5 v5.2.0
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- gRPC/qrcode_microservice/go_server/logging.go
+++ gRPC/qrcode_microservice/go_server/logging.go
@@ -1,6 +1,11 @@
 package main
 
-import "context"
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
 
 type loggingService struct {
 	next JwtGenerator
@@ -9,9 +14,9 @@ type loggingService struct {
 func (s *loggingService) GenerateJWT(ctx context.Context, session string) (string, error) {
 	defer func(begin time.Time) {
 		logrus.WithFields(logrus.Fields{
-			"took": time.Since(begin),
-			"error": err,
+			"took":    time.Since(begin),
+			"error":   err,
 			"session": session,
 		})
-	}
+	}(time.Now())
 }
